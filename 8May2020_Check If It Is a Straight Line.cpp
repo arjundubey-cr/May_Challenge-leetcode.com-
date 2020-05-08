f02a9108ea902251f0adfcbc35f571921597bcd1
Arjun Dubey <40758425+arjundubey-cr@users.noreply.github.com>
2020-05-08 13:10:47 +0530

    Update 8May2020_Check If It Is a Straight Line.cpp

--- 8May2020_Check If It Is a Straight Line.cpp
+++ 8May2020_Check If It Is a Straight Line.cpp
@@ -14,8 +14,8 @@ public:
         for(int i=0;i<coordinates.size()-2;i++)
         {
             
-            int x1 = coordinates[i][0], x2 =  coordinates[i+1][0], x3 =  coordinates[i+2][0],
-            y1 =  coordinates[i][1], y2 =  coordinates[i+1][1], y3 =  coordinates[i+2][1];
+            int x1 = coordinates[i][0], x2 =  coordinates[i+1][0], x3 =  coordinates[i+2][0];
+            int y1 =  coordinates[i][1], y2 =  coordinates[i+1][1], y3 =  coordinates[i+2][1];
             
             if(collinear(x1, y1, x2, y2, x3, y3)==0)return false; 
         }
